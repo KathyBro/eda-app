8cb94f2b9ef604c96618b897c90761e8a68f6028
KathyBro <randomizerkatt@gmail.com>
2024-11-22 17:42:26 -0700

    Added some endpoints for schedule and a practice command

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schedule import Schedule, RoomSchedule, Lesson
 
 app = FastAPI()
 
@@ -6,6 +7,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/schedule")
+def get_schedule():
+    schedule = Schedule()
+    return schedule
+
+
+@app.post("/schedule")
+def create_room(room: RoomSchedule):
+    return room
 
 
 if __name__ == "__main__":  # pragma: no cover
--- src/cli.py
+++ src/cli.py
@@ -6,5 +6,19 @@ def main():
     print("Hello, World!")
 
 
+@click.command()
+@click.option("--count", default=1, help="Number of greetings.")
+@click.option("--name", prompt="Your name", help="The person to greet.")
+def hello(count, name):
+    for x in range(count):
+        click.echo("Hello %s!" % name)
+
+
+@click.command()
+@click.option()
+def create_lesson(days, start, end, name):
+    pass
+
+
 if __name__ == "__main__":  # pragma: no cover
     main()
